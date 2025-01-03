6975f1f623f9dc6966ba8fc2c91c858428649ce8
Sebastian Liévre <sebastian.lievre@gmx.de>
2025-01-03 09:19:48 +0000

    02 / Deklaration + Wertzuweisung I Update

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -16,4 +16,4 @@ console.log(firstName); // Ausgabe
 firstName = "Max"; // Wertzuweisung | assignment
 console.log(firstName); // Ausgabe
 let familyName = "Mütze"; // Deklaration + Wertzuweisung 
-console.log("Hi, ich bin " + firstName " " + familyName + "!"); // Ausgabe
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausgabe
