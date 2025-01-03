dde1cad1c4ec5f387aff8672d6f377a73805ee31
Sebastian Liévre <sebastian.lievre@gmx.de>
2025-01-03 09:17:41 +0000

    02 / Deklaration + Wertzuweisung I

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -3,3 +3,17 @@
 /*
   Blockkommentar   
  */
+
+  /***** Variablen 01 *******/
+
+//   console.log("Hi!"); working
+//   console.log(Hi); not working
+
+  /* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung 
+console.log("Hi, ich bin " + firstName " " + familyName + "!"); // Ausgabe
